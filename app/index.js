c3d553ff764edd7dd210fbaf6e47dc6dae9a75b8
Christiana Koudigkeli <christiana.koudigkeli@nokia.com>
2018-05-23 15:22:24 +0300

    body-parser for post

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+
+app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
