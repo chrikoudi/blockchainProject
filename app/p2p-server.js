b3daeb538bf3e5502bf7a548218492186a9c17e5
Christiana Koudigkeli <christiana.koudigkeli@nokia.com>
2018-05-24 15:46:44 +0300

    WIP -P2P server

---
+++ app/p2p-server.js
@@ -0,0 +1,10 @@
+const Websocket = require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+class P2pServer {
+    constructor(blockchain) {
+        this.blockchain = blockchain;
+    }
+}
