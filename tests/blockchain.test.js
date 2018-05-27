b684fe66c0e2b525c56ed33bfde7acaf88594aa3
Christiana Koudigkeli <ckoudigkeli@gmail.com>
2018-05-27 11:01:06 +0300

    typo fixed

--- tests/blockchain.test.js
+++ tests/blockchain.test.js
@@ -1,5 +1,5 @@
 const Blockchain = require('../blockchain/index');
-const Block = require('../blockchain/index');
+const Block = require('../blockchain/block');
 
 describe('Blockchain', () => {
     let bc, bc2;
